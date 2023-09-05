c50d0d504d0ad6e3b840b2a59826db12bde564c3
Habtamu <habtamuwolde96@gmail.com>
2023-09-05 02:51:52 -0700

    fixed more than two print function error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    uppercase = 0
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(ord(i) - 32)), end="")
+            uppercase = 32
         else:
-            print("{}".format(i), end="")
+            uppercase = 0
+        print("{}".format(chr(ord(i) - uppercase)), end="")
     print()
