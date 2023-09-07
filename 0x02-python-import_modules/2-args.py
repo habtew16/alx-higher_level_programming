57bea718ad7170d691857f828ac71d81b208981c
Habtamu <habtamuwolde96@gmail.com>
2023-09-07 09:24:02 -0700

    add from args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv[1:]
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
 
-if len(argv) == 0:
-    print(f"{0}");
-else:
-    sum = 0
-    for i in argv:
-        sum += int(i)
-    print(f"{sum}")
+    if len(argv) == 0:
+        print(f"{0}");
+    else:
+        sum = 0
+        for i in argv:
+            sum += int(i)
+        print(f"{sum}")
