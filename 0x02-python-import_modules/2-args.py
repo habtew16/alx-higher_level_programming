935af96350cccee7ea045c5ec95ae5f3224bdbe5
Habtamu <habtamuwolde96@gmail.com>
2023-09-07 09:51:12 -0700

    fix pycode style error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,9 @@ if __name__ == "__main__":
         print("{} arguments.".format(argc - 1))
     elif argc == 2:
         print("{} argument:".format(argc - 1))
-        for index,item in enumerate(argv[1:]):
+        for index, item in enumerate(argv[1:]):
             print("{}: {}".format(index + 1, item))
     else:
         print("{} arguments:".format(argc - 1))
-        for index,item in enumerate(argv[1:]):
+        for index, item in enumerate(argv[1:]):
             print("{}: {}".format(index + 1, item))
-
