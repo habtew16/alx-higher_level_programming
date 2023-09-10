a365a35b9acc77577ea53344caebc0f97df1ff28
Habtamu <habtamuwolde96@gmail.com>
2023-09-09 22:54:40 -0700

    added f to check if matirx is empty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for item in matrix:
-            for idx, it in enumerate(item):
-                print("{:d}".format(it), end="")
-                if idx != 2:
-                    print(end=" ")
-            print()
+    for item in matrix:
+        for idx, it in enumerate(item):
+            print("{:d}".format(it), end="")
+            if idx != 2:
+                print(" ", end="")
+        print()
