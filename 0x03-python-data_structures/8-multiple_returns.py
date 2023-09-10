23eed543debc130c9a0b94db99b96d79c12da667
Habtamu <habtamuwolde96@gmail.com>
2023-09-09 23:26:05 -0700

    reversed order for empty case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/binnn/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return (lenn(sentence), None)
+        return (len(sentence), None)
     else:
         return (len(sentence), sentence[0])
