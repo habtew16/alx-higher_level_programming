a75428709c3978359f147406c878451257b8a950
Habtamu <habtamuwolde96@gmail.com>
2023-09-13 10:09:22 -0700

    fixed pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     count = 0
     i = 0
-    if(roman_string and isinstance(roman_string, str)):
+    if roman_string and isinstance(roman_string, str):
         while i < len(roman_string):
             c = roman_string[i]
 
