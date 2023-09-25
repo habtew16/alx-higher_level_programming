829bf5b9c2d8f1b0213d62e36f5b2044dc7054d6
Habtamu <habtamuwolde96@gmail.com>
2023-09-25 10:17:03 -0700

    fixed ssome error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,16 +2,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            result = 0
-            print("division by 0")
+            print("{}".format("division by 0"))
         except TypeError:
-            result = 0
-            print("wrong type")
+            print("{}".format("wrong type"))
         except IndexError:
-            result = 0
-            print("out of range")
-        new_list.append(result)
+            print("{}".format("out of range"))
+        finally:
+            new_list.append(result)
     return new_list
