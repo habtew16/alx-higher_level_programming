40a7ff4dac78ebff94ed36f82bc8dfd89c91cf24
Habtamu <habtamuwolde96@gmail.com>
2023-09-26 09:03:36 -0700

    2 square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,15 +5,17 @@
 class Square:
     """class square with private instanec"""
     def __init__(self, size=0):
-        self.__size = size
         if(type(size) is not int):
             raise TypeError("size must be an integer")
         if(size < 0):
             raise ValueError("size must be >= 0")
+
+        self.__size = size
     """
     init is method which is called when
     class object is created and initiates
     private size
     args:
         __size(int): the size of the square
+        size must be positive
     """
