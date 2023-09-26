6de97b5ca20a621387ec799833618ca57fc0704a
Habtamu <habtamuwolde96@gmail.com>
2023-09-26 04:39:39 -0700

    0-sqaure

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 class Square:
+    """creates a square class object
+
+    for now this Square class object
+    is not doing anything
+
+    Note:
+        do not include the 'self' paramter
+        in args
+
+    """
     pass
-    """
-    creates a square class object
-    """
