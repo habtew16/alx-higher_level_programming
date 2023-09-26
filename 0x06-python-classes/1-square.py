39561656cadbc9f1ae5d7be6a5b78db9794f461d
Habtamu <habtamuwolde96@gmail.com>
2023-09-26 08:38:11 -0700

    square ith size of private story

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """class square with private instanec"""
+    def __init__(self, size):
+        self.__size = size
+    """
+    init is method which is called when
+    class object is created and initiates
+    private size
+    args:
+        __size(int): the size of the square
+    """
