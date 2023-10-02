7a8241f64643d9ecd0e45276da5d58e98d0db945
Habtamu <habtamuwolde96@gmail.com>
2023-10-02 09:34:35 -0700

    define rectangle that getts width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"Rectangle class"
+
+
+class Rectangle:
+    """
+    this is rectangle class that does nothing
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """
+    width private data setter
+    """
+    @property
+    def width(self):
+        return self.__width
+
+    """
+    width data getter
+    """
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    """
+    height getter
+    """
+    @property
+    def height(self):
+        return self.__height
+
+    """
+    height setter
+    """
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
