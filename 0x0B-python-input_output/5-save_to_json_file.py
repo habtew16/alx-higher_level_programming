99ed1af2bcfeab9fcaf8cded11f79d61e7a98583
Habtamu <habtamuwolde96@gmail.com>
2023-10-11 00:53:12 -0700

    save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""file handling"""
+
+
+def save_to_json_file(my_obj, filename):
+    """function to save to json"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(json.dumps(my_obj))
