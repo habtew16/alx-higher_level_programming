fcc4d41dad91d5222edb1f465c88f4afd984fb43
Habtamu <habtamuwolde96@gmail.com>
2023-10-11 01:18:02 -0700

    fixed erro with file opeining

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """function to load from json"""
-    with open(filename, mode='r', encoding="utf-8"):
+    with open(filename, mode='r', encoding="utf-8") as f:
         data = json.load(f)
     return data
