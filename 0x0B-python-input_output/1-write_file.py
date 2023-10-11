0d6202da27c5ccdd9fac987a52ec61cddebffc88
Habtamu <habtamuwolde96@gmail.com>
2023-10-11 00:30:19 -0700

    fixed return

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """funnction to write on file"""
     with open(filename, mode='w', encoding="utf-8") as f:
         num_chars = f.write(text)
-    return num_chars + 5
+    return num_chars
