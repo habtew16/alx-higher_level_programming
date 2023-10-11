331b3a62d47a6ba8e7ed7fa1aef403b7ca690009
Habtamu <habtamuwolde96@gmail.com>
2023-10-10 23:33:05 -0700

    write on file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""file handling"""
+
+
+def write_file(filename="", text=""):
+    """funnction to write on file"""
+    with open(filename, mode='w', encoding="utf-8") as f:
+        f.write(text)
