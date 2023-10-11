d24f2ec30c288b8762db12fc2ca69549a40a4750
Habtamu <habtamuwolde96@gmail.com>
2023-10-11 01:25:49 -0700

    add ite to json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,11 +5,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    jsonList = load_from_json_file("add_item.json")
-except:
-    jsonList = []
-
+jsonList = load_from_json_file("add_item.json")
 for i in sys.argv[1:]:
     jsonList.append(i)
 save_to_json_file(jsonList, "add_item.json")
