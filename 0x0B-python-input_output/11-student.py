e28fc7d95c16652c6682a2a74f1c277ea657417e
Habtamu <habtamuwolde96@gmail.com>
2023-10-11 01:56:23 -0700

    11 student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""file handling"""
+"""conversion of json in clas"""
 
 
 class Student:
-    """student class to retrieve JSON of self"""
+    """init funciton"""
     def __init__(self, first_name, last_name, age):
-        """init function"""
+        """initializer function to assign data"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves JSON of self"""
-        if attrs is None:
-            return self.__dict__
-
-        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
-            return {x: y for x, y in self.__dict__.items() if x in attrs}
+        """function that converts self to json"""
+        if(attrs and isinstance(attrs, list) and
+           all(isinstance(x, str) for x in attrs)):
+            return({x: y for x, y in self.__dict__.items() if x in attrs})
         else:
             return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces allattributes with json"""
+        if(json):
+            self.__dict__ = json
