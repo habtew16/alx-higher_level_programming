b6e388ec9df723b266a3676d622ead9d2441a002
Habtamu <habtamuwolde96@gmail.com>
2023-10-11 01:17:02 -0700

    fixed variable name

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     """function to load from json"""
     with open(filename, mode='r', encoding="utf-8"):
-        ff = json.load(f)
-    return ff
+        data = json.load(f)
+    return data
