9d74e6fd04a67f45f0e57ed0c43e6c8418dc5cf4
Habtamu <habtamuwolde96@gmail.com>
2023-10-13 12:16:17 -0700

    text indeint

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""text indentation function"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    start = 0
+    for idx, val in enumerate(text):
+        if val in '?:.':
+            print(text[start:idx + 1].strip() + '\n')
+            start = idx + 1
+    if not start:
+        print(text, end='')
+    elif start is not len(text):
+        print(text[start:idx + 1].strip(), end='')
