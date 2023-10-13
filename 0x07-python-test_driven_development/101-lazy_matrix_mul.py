0a2b1fc463a584bed2825c2a753b9a08ad08c3ba
Habtamu <habtamuwolde96@gmail.com>
2023-10-13 13:03:05 -0700

    lazy matrix

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""numpy multiply matrix"""
+
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """function to mu;tply matrix with numpy"""
+    return numpy.matmul(m_a, m_b)
