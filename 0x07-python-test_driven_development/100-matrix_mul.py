cdb67e99d347780ce1bf67530ad9c53ad05f4fad
Habtamu <habtamuwolde96@gmail.com>
2023-10-13 12:44:26 -0700

    ixed typeerror

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -23,9 +23,9 @@ def matrix_mul(m_a, m_b):
                                  for ele in listx] for listx in m_b]):
         raise TypeError('m_b should contain only integers or floats')
     if len(set(len(listx) for listx in m_a)) > 1:
-        raise TypeError('each row of m_a must should be of the same size')
+        raise TypeError('each row of m_a must be of the same size')
     if len(set(len(listx) for listx in m_b)) > 1:
-        raise TypeError('each row of m_b must should be of the same size')
+        raise TypeError('each row of m_b must be of the same size')
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
     return([[sum(a*b for a, b in
