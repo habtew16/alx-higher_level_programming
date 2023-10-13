a6c3759e38731482ea91e3b1f4d2fab600bc487b
Habtamu <habtamuwolde96@gmail.com>
2023-10-13 11:35:31 -0700

    ixed two lines before start of function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 function to print user full name
 """
 
+
 def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
