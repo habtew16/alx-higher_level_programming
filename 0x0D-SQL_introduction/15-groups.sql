46ed0b09ef520f7928c09ad993002e9b48c394d1
Habtamu <habtamuwolde96@gmail.com>
2023-11-14 02:58:05 -0800

    made it to be in two lines

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- get count of the score
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score
+SELECT score, COUNT(score) AS number
+FROM second_table GROUP BY score ORDER BY score
