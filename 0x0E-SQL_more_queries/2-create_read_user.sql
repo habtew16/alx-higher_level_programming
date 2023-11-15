6d54a235c4c68ef93b1c342e977c0b7a2a7cdf96
Habtamu <habtamuwolde96@gmail.com>
2023-11-15 04:40:06 -0800

    create user and database

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- create database if nnot exists
+-- also create user i doesn't exsit
+-- with only read permsission
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
