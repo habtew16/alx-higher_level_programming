6be2a713bf3cd1becf110a903c14ae907619de4a
Habtamu <habtamuwolde96@gmail.com>
2023-11-15 11:27:01 -0800

    fixing

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- 14-my_genres.sqlSELECT tgg.name FROM tv_genres AS tgg
-JOIN tv_show_genres AS tgs
-ON tgs.genre_id = tgg.id
+JOIN tv_show_genres AS name
+ON name.genre_id = tgg.id
 JOIN tv_shows AS ts
-ON ts.id = tgs.show_id
+ON ts.id = name.show_id
 WHERE ts.title='Dexter'
 GROUP BY tgg.name ASC;
