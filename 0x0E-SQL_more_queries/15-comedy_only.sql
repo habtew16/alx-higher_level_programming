6207a054f0884001c01106a97e837abb2675ac0a
Habtamu <habtamuwolde96@gmail.com>
2023-11-15 11:30:41 -0800

    only commedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- 15-comedy_only.sql
+SELECT ts.title FROM tv_shows AS ts
+JOIN tv_show_genres AS tgs
+ON ts.id = tgs.show_id
+JOIN tv_genres AS tgg
+ON tgs.genre_id = tgg.id
+WHERE tgg.name = 'Comedy'
+GROUP BY ts.title ASC;
