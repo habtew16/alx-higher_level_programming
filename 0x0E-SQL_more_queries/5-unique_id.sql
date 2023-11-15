fa0d5bcb6ee7bbf4ce66e6fcd169fe4f2695a22f
Habtamu <habtamuwolde96@gmail.com>
2023-11-15 04:56:48 -0800

    correced tbale name

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- create table named id not null
-CREATE TABLE IF NOT EXISTS unique_id(id INT NOT NULL DEfAULT 1 AUTO_INCREMENT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT NOT NULL DEfAULT 1, name VARCHAR(256), UNIQUE(`id`));
