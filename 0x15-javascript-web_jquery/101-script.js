e777454b372dfd8582b5ba45081ee1de4feafa55
= <=>
2024-02-07 00:44:44 -0800

    list remove add element

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+      $('#add_item').on('click', function() {
+        $('ul.my_list').append('<li>Item</li>');
+      });
+
+      $('#remove_item').on('click', function() {
+        $('ul.my_list li:last-child').remove();
+      });
+
+      $('#clear_list').on('click', function() {
+        $('ul.my_list').empty();
+      });
+    });
