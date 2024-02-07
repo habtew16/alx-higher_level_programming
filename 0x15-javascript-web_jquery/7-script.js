f5f5459977034ada211c9459bc2d1b4c741848fd
= <=>
2024-02-07 00:36:54 -0800

    star wars character

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,19 @@
-$('document').ready(function () {
-  $.getJSON('https://swapi.co/api/people/5/?format=json', function (value) {
-    $('DIV#character').text(value.name);
+// $('document').ready(function () {
+//   $.getJSON('https://swapi.co/api/people/5/?format=json', function (value) {
+//     $('DIV#character').text(value.name);
+//   });
+// });
+$(document).ready(function() {
+    $.ajax({
+      url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        let characterName = data.name;
+        $('#character').text(characterName);
+      },
+      error: function(error) {
+        console.error('Error fetching character data:', error);
+      }
+    });
   });
-});
